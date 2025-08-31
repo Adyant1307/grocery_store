5e1c31e239514eced3daaa79c322ed8bd6ca42b3
Adyant1307 <adyantkumarverma@gmail.com>
2025-08-31 16:33:10 +0530

    updated

--- backend/mysql_connect.py
+++ backend/mysql_connect.py
@@ -4,3 +4,4 @@ cnx = mysql.connector.connect(user='root', password='Qwerty@123',
                               host='127.0.0.1',
                               database='grocery_store')
 cnx.close()
+[]
